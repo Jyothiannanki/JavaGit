5c246701c156166840b213733808da750e627117
Jyothi Annanki <jyothiannanki000@gmail.com>
2022-09-30 10:56:27 +0530

    original file

--- nextfile.java
+++ nextfile.java
@@ -2,5 +2,8 @@ public class nextfile {
     public static void main(String[] args) {
         System.out.println("hai nextfile");
         System.out.println("changed file");
+        System.out.println("merging file in javagitbranch");
+
+
     }
 }
