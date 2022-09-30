7fa22887104970858f4b9475a3f161c9f75420c6
Jyothi Annanki <jyothiannanki000@gmail.com>
2022-09-30 12:05:46 +0530

    Adding textpro filr

---
+++ textpro.java
@@ -0,0 +1,9 @@
+public class textpro {
+    public static void main(String[] args) {
+        System.out.println("hai nextfile");
+        System.out.println("changed file");
+        System.out.println("merging file in master");
+
+
+    }
+}
