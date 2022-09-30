c492ff5b47bed6c2ac4638e3abf66537b72ed45a
Jyothi Annanki <jyothiannanki000@gmail.com>
2022-09-30 11:01:18 +0530

    original file

--- nextfile.java
+++ nextfile.java
@@ -2,5 +2,6 @@ public class nextfile {
     public static void main(String[] args) {
         System.out.println("hai nextfile");
         System.out.println("changed file");
+        System.out.println("changed file in javagitbranch");
     }
 }
